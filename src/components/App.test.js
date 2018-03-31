2cbf833ad40059c27f244d272b6d58592718b274
Matt Burstein <j.matthew.burstein@gmail.com>
2018-03-31 19:06:24 +0100

    feat: add setBalance action

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,3 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+
+describe('App', () => {
+  const app = shallow(<App />);
+
+  it('render correctly', () => {
+    expect(app).toMatchSnapshot();
+  });
+});
