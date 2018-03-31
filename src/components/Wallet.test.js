f9eb17b0e5b06f9a0a97cc79c04e4b26d75d8b00
Matt Burstein <j.matthew.burstein@gmail.com>
2018-03-31 23:13:19 +0100

    feat: add deposit and withdraw to ui

--- src/components/Wallet.test.js
+++ src/components/Wallet.test.js
@@ -3,8 +3,10 @@ import { shallow } from 'enzyme';
 import { Wallet } from './Wallet';
 
 describe('Wallet', () => {
-  const props = { balance: 20 };
-  const wallet = shallow(<Wallet {...props} />)
+  const mockDeposit = jest.fn();
+  const mockWithdraw = jest.fn();
+  const props = { balance: 20, deposit: mockDeposit, withdraw: mockWithdraw };
+  const wallet = shallow(<Wallet {...props} />);
 
   it('renders properly', () => {
     expect(wallet).toMatchSnapshot();
@@ -12,5 +14,43 @@ describe('Wallet', () => {
 
   it('displays the balance from props', () => {
     expect(wallet.find('.balance').text()).toEqual('Wallet balance: 20');
+  });
+
+  it('creates an input to deposit into or withdraw from the balance', () => {
+    expect(wallet.find('.input-wallet').exists()).toBe(true);
+  });
+
+  describe('when the user types into the wallet input', () => {
+    const userBalance = '25';
+    const userBalanceInt = parseInt(userBalance, 10);
+
+    beforeEach(() => {
+      wallet.find('.input-wallet')
+        .simulate('change', { target: { value: userBalance } });
+    });
+
+    it('updates the local wallet balance in `state` and converts it to a number', () => {
+      expect(wallet.state().balance).toEqual(userBalanceInt);
+    });
+
+    describe('and the user wants to make a deposit', () => {
+      beforeEach(() => {
+        wallet.find('.btn-deposit').simulate('click');
+      });
+
+      it('dispatches the `deposit()` it receives from props with local balance', () => {
+        expect(mockDeposit).toHaveBeenCalledWith(userBalanceInt);
+      });
+    });
+
+    describe('and the user wants to withdraw', () => {
+      beforeEach(() => {
+        wallet.find('.btn-withdraw').simulate('click');
+      });
+
+      it('dispatches the `withdraw()` it receives from props with local balance', () => {
+        expect(mockWithdraw).toHaveBeenCalledWith(userBalanceInt);
+      });
+    });
   });
 });
